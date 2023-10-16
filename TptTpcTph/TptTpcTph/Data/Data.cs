0e1803359e016e6f3344a79cd0ef04b8b461bb3a
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-16 08:32:46 +0330

    .

---
+++ TptTpcTph/TptTpcTph/Data/Data.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TptTpcTph.Models;
+
+namespace TptTpcTph.Data
+{
+    public class Data : DbContext
+    {
+        public Data(DbContextOptions<Data> opt) : base(opt)
+        {
+            
+        }
+        public DbSet<Player> players { get; set; }
+        public DbSet<Person> MyProperty { get; set; }
+        public DbSet<Metric> Metrics { get; set; }
+    }
+}
--- TptTpcTph/TptTpcTph/Program.cs
+++ TptTpcTph/TptTpcTph/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace TptTpcTph
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+
+namespace TptTpcTph
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            var builder = WebApplication.CreateBuilder(args);
+
         }
     }
 }
