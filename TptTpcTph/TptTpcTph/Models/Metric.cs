f65698c978ab7efc2c1bcf13d54f2d7525053fb2
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-16 08:24:17 +0330

    Project Added + Models Created

---
+++ TptTpcTph/TptTpcTph/Models/Metric.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TptTpcTph.Models
+{
+    public class Metric
+    {
+        public int Id { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+    }
+}
---
+++ TptTpcTph/TptTpcTph/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TptTpcTph.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string? TeamName { get; set; }
+        public string? Position { get; set; }
+    }
+}
